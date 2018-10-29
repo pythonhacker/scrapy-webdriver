706d275b3cd63ca4efe7df41c90cf1360e9e94f6
Anand B Pillai <anandpillai@letterboxes.org>
2018-10-29 11:27:25 +0530

    Fixng setup.py script

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ import sys
 
 from scrapy_webdriver import metadata
 
-# Auto-install and download distribute.
-import distribute_setup
-distribute_setup.use_setuptools()
-
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
